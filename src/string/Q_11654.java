aab35fbda5ccfe93e053a735ff4e7b84044a516d
2SunE <tjsrhkdjaak@gmail.com>
2020-12-22 01:44:30 +0900

    백준 알고리즘 11654번 - 아스키 코드
    문제 해결

---
+++ src/string/Q_11654.java
@@ -0,0 +1,12 @@
+package string;
+
+import java.util.Scanner;
+
+public class Q_11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		int b = a.charAt(0);
+		System.out.println(b);
+	}
+}
